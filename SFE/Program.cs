b7ecfb43854c4252576afe51f5a8f4a9c6be38c3
jyk8904 <jyk8904@naver.com>
2019-12-18 07:53:51 +0900

    test

--- SFE/Program.cs
+++ SFE/Program.cs
@@ -14,6 +14,7 @@ namespace SFE
         [STAThread]
         static void Main()
         {
+            ///ddddddtest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
